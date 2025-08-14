c4fb9e0339c9d8fe33467429cd324ab2fa8c3d25
Fabio <Fabio@FABIO-PC>
2025-08-14 20:29:11 -0300

    fix service address resolution

--- Src/01.Presentation/TreeNotes.UI/js/defaults.js
+++ Src/01.Presentation/TreeNotes.UI/js/defaults.js
@@ -3,7 +3,7 @@ window._env_ = {
   TREE_NOTES_SERVICE_URL: "REPLACE_TREE_NOTES_SERVICE_URL"
 };
 
-const API_BASE_URL = (window._env_.TREE_NOTES_SERVICE_URL.indexOf("_TREE_NOTES_SERVICE_URL") >= 0 ?  window._env_.TREE_NOTES_SERVICE_URL 
+const API_BASE_URL = (window._env_.TREE_NOTES_SERVICE_URL.indexOf("_TREE_NOTES_SERVICE_URL") < 0 ?  window._env_.TREE_NOTES_SERVICE_URL 
                                                                                                : "http://localhost:5100");
 
 toastr.options = {
