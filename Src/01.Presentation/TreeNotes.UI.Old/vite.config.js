ec1ebcdbceb3158b95692716e6091663e032be78
Fabio <Fabio@FABIO-PC>
2025-08-14 16:20:31 -0300

    Renaming TreeNotes.UI for TreeNotes.UI.Old

--- Src/01.Presentation/TreeNotes.UI.Old/vite.config.js
+++ Src/01.Presentation/TreeNotes.UI.Old/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5555',
+        target: 'http://localhost:5100',
         changeOrigin: true,
       }
     }
