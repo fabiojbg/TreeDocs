976062f6e212da0b7a624a41e9bc965a5d8469ca
Fabio <Fabio@FABIO-PC>
2025-08-16 14:11:19 -0300

    Remove useless item from tollbar

--- Src/01.Presentation/TreeNotes.UI/js/dashboard.js
+++ Src/01.Presentation/TreeNotes.UI/js/dashboard.js
@@ -158,7 +158,6 @@ $(function() {
                     [{ 'list': 'ordered'}, { 'list': 'bullet' }],
                     [{ 'script': 'sub'}, { 'script': 'super' }],
                     [{ 'indent': '-1'}, { 'indent': '+1' }],
-                    [{ 'direction': 'rtl' }],
                     [{ 'size': ['small', false, 'large', 'huge'] }],
                     [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
                     [{ 'color': [] }, { 'background': [] }],
